e14feb45d772f04529e36ed3ba13eac3506fc394
Alexandr <blackvice@yandex.ru>
2023-02-16 18:48:06 +0300

    Initial commit

---
+++ HW.py
@@ -0,0 +1,17 @@
+# n = int(input())
+# m = int(input())
+#
+# def r(a,b):
+#     if b == 0:
+#         return 1
+#     return r(a, b - 1) * a
+# print(r(n, m))
+
+# n = int(input())
+# m = int(input())
+#
+# def sum(a,b):
+#     if b == 0:
+#         return 1
+#     return sum(a, b - 1) + 1
+# print(sum(n, m))
